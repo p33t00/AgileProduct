3721883365ec5c88b661b4eb1f9b333f86235ba0
p33t00 <saip8tha@gmail.com>
2022-04-11 16:12:39 +0200

    Multiple bean fethc fix

--- src/test/java/com/hkrsdgroup/agileproduct/DBConnectTest.java
+++ src/test/java/com/hkrsdgroup/agileproduct/DBConnectTest.java
@@ -2,6 +2,7 @@ package com.hkrsdgroup.agileproduct;
 
 import com.hkrsdgroup.agileproduct.beans.TestBean;
 import org.apache.commons.dbutils.handlers.BeanHandler;
+import org.apache.commons.dbutils.handlers.BeanListHandler;
 import org.junit.jupiter.api.Order;
 import org.junit.jupiter.api.Test;
 import org.sqlite.SQLiteDataSource;
@@ -10,6 +11,7 @@ import java.sql.Connection;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
+import java.util.List;
 import java.util.ResourceBundle;
 
 import static org.junit.jupiter.api.Assertions.*;
@@ -53,8 +55,47 @@ public class DBConnectTest {
         // inserting dummy data
         dbc.updateRawQuery("INSERT INTO testing (test_field) VALUES ('" + assertVal + "');");
         // Testing getEntity()
-        TestBean tb = dbc.getEntity("SELECT * FROM testing", new BeanHandler<TestBean>(TestBean.class));
+        TestBean tb = dbc.getEntity("SELECT * FROM testing LIMIT 1;", new BeanHandler<>(TestBean.class));
         assertEquals(assertVal, tb.getTest_field());
+        // Clean up
+        dropTestTable(dbc);
+    }
+
+    @Test
+    @Order(4)
+    void shouldGetEntityByArgs() {
+        String assertVal = "Hallo world";
+        DBConnect dbc = new DBConnect(rb.getString("dsn"));
+        // init DB
+        createTestTable(dbc);
+        // inserting dummy data
+        dbc.updateRawQuery("INSERT INTO testing (test_field) VALUES ('" + assertVal + "');");
+        // Testing getEntity()
+        TestBean tb = dbc.getEntity(
+                "SELECT * FROM testing WHERE id = ? LIMIT 1;",
+                new BeanHandler<>(TestBean.class),
+                1);
+        assertEquals(assertVal, tb.getTest_field());
+        // Clean up
+        dropTestTable(dbc);
+    }
+
+    @Test
+    @Order(5)
+    void shouldGetEntityList() {
+        String assertVal1 = "Hallo world";
+        String assertVal2 = "Bye world";
+        DBConnect dbc = new DBConnect(rb.getString("dsn"));
+        // init DB
+        createTestTable(dbc);
+        // inserting dummy data
+        dbc.updateRawQuery("INSERT INTO testing (test_field) VALUES ('" + assertVal1 + "');");
+        dbc.updateRawQuery("INSERT INTO testing (test_field) VALUES ('" + assertVal2 + "');");
+        // Testing getEntity() List
+        List<TestBean> tb = dbc.getEntity("SELECT * FROM testing;", new BeanListHandler<>(TestBean.class));
+        assertEquals(assertVal1, tb.get(0).getTest_field());
+        assertEquals(assertVal2, tb.get(1).getTest_field());
+
         // Clean up
         dropTestTable(dbc);
     }
